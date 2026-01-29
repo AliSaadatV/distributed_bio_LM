4e49a4d56c717a9c74b11566c8d931a45ffe5516
AliSaadatV <ali.saadat@epfl.ch>
2026-01-29 10:09:08 +0100

    cleaned up folders

--- not_used/sanity.py
+++
@@ -1,7 +0,0 @@
-import os, torch
-print(
-    "rank", os.environ.get("RANK"),
-    "local_rank", os.environ.get("LOCAL_RANK"),
-    "cuda", torch.cuda.is_available(),
-    "device_count", torch.cuda.device_count()
-)
